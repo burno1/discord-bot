f0ca614eef0de3b2420fea667b465b7f53147691
Lucas Honda <lucashondastts@gmail.com>
2021-09-14 14:38:09 -0300

    Add new command queue

---
+++ commands/queue.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: "queue",
+    description: "Get the list of songs.",
+    execute(message) {
+        const serverQueue = message.client.queue.get(message.guild.id);
+
+        if (!serverQueue) return message.channel.send('There is nothing playing.');
+        else {
+            let msg = Discord.MessageEmbed();
+            serverQueue.songs.forEach(song=> {
+                msg.addField(song.title)
+            });
+            return message.channel.send(msg);
+        }
+        
+    }
+}
